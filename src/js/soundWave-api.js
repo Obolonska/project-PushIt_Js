70965be52bbccaae7c01dc9961f7858717920f7c
anastasiia_obolonska <velentiy@gmail.com>
2025-06-11 18:48:02 +0300

    Artist

--- src/js/soundWave-api.js
+++ src/js/soundWave-api.js
@@ -15,12 +15,35 @@
 //     throw error;
 //   }
 // }
+// async function fetchArtists(name, sort, genre, page = 1, limit = 8) {
+//   try {
+//     const baseUrl = 'https://sound-wave.b.goit.study/api/artists';
+//     const params = new URLSearchParams();
+//     if (name) params.append('name', name);
+//     if (sort) params.append('sortName', sort);
+//     if (genre) params.append('genre', genre);
+//     params.append('page', page);
+//     params.append('limit', limit);
 
-export async function getCards(page) {
+//     const response = await axios.get(`${baseUrl}?${params.toString()}`);
+//     console.log('Артисти отримані:', response.data);
+
+//     // const artists = response.data;
+//     // renderArtists(artists);
+//   } catch (error) {
+//     console.error('Помилка запиту артистів:', error.message);
+//   }
+// }
+export async function getCards(name, sort, genre, page = 1, limit = 8) {
   try {
-    const response = await fetch(
-      `https://sound-wave.b.goit.study/api/artists?limit=8&page=${page}`
-    );
+    const baseUrl = 'https://sound-wave.b.goit.study/api/artists';
+    const params = new URLSearchParams();
+    if (name) params.append('name', name);
+    if (sort) params.append('sortName', sort);
+    if (genre) params.append('genre', genre);
+    params.append('page', page);
+    params.append('limit', limit);
+    const response = await fetch(`${baseUrl}?${params.toString()}`);
 
     if (!response.ok) {
       const text = await response.text();
