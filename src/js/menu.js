15f76cec41419f567dacd7f2b2a58d1717adf9b3
Lina <linamark0001@gmail.com>
2025-06-12 22:17:28 +0300

    fix close

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,53 +1,28 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-menu-open]'),
-      closeModalBtn: document.querySelector('[data-menu-close]'),
-      links:document.querySelectorAll('.mob-navig .mobile-menu-link'),
-      modal: document.querySelector('[data-modal]'),
-    };
-  
-    console.log(refs.closeModalBtn);
-    refs.openModalBtn.addEventListener('click', openModalBtn);
-    refs.closeModalBtn.addEventListener('click', closeModalBtn);
-    refs.links.forEach(link =>{
-      console.log(link);
-      link.addEventListener('click', onClickMenuItem);
-    
-    })
+  const refs = {
+    openBtn: document.querySelector('[data-menu-open]'),
+    closeBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-modal]'),
+    menuLinks: document.querySelectorAll('[data-menu-item]'),
+  };
 
-    function closeModalBtn() {
-    refs.modal.classList.add('is-hidden')
-    refs.openModalBtn.classList.remove('is-hidden')
-    refs.closeModalBtn.classList.add('is-hidden')
-    
+  refs.openBtn.addEventListener('click', onOpenMenu);
+  refs.closeBtn.addEventListener('click', onCloseMenu);
+  refs.menuLinks.forEach(link => {
+    link.addEventListener('click', onCloseMenu);
+  });
+
+  function onOpenMenu() {
+    refs.menu.classList.remove('is-hidden');
+    refs.openBtn.classList.add('is-hidden');
+    refs.closeBtn.classList.remove('is-hidden');
+    document.body.classList.add('menu-open');
   }
-  
-    function openModalBtn(){
-        console.log(refs.modal)
-        refs.modal.classList.remove('is-hidden')
-        refs.openModalBtn.classList.add('is-hidden')
-        refs.closeModalBtn.classList.remove('is-hidden')
-    }
 
-    function onClickMenuItem(event){
-      refs.modal.classList.add('is-hidden')
-      refs.openModalBtn.classList.remove('is-hidden')
-      refs.closeModalBtn.classList.add('is-hidden')
-    
-      
-    }
-  })();
-
-const openBtn = document.querySelector('[data-menu-open]');
-const closeBtn = document.querySelector('[data-menu-close]');
-const menu = document.querySelector('[data-modal]');
-
-openBtn.addEventListener('click', () => {
-  menu.classList.remove('is-hidden');
-  document.body.classList.add('menu-open');
-});
-
-closeBtn.addEventListener('click', () => {
-  menu.classList.add('is-hidden');
-  document.body.classList.remove('menu-open');
-});
+  function onCloseMenu() {
+    refs.menu.classList.add('is-hidden');
+    refs.openBtn.classList.remove('is-hidden');
+    refs.closeBtn.classList.add('is-hidden');
+    document.body.classList.remove('menu-open');
+  }
+})();
